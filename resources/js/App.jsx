1acbd3c3c91b9e1733e16b85741245fcf26280b6
Dzaka Al <dzakaal10@gmail.com>
2025-12-06 22:22:40 +0700

    Add UI for Dokumen Legalitas

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Beranda from "./pages/beranda/Beranda";
 import Profil from "./pages/profil/ProfilSaya";
+import Dokumen from "./pages/dokumen/DokumenLegalitas";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/beranda" element={<Beranda />} />
         <Route path="/profil" element={<Profil />} />
+        <Route path="/dokumen" element={<Dokumen />} />
       </Routes>
     </Router>
   );
